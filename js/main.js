6f67accd4d88dffe13835511f1d24812c84102cf
max bittker <maxbittker@gmail.com>
2015-09-03 23:48:55 -0400

    img

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-    $('.navbar-nav li a').each(function(index) {
-        if ($(this).attr('href') === "."+window.location.pathname) {
-            $(this).parent().addClass("active");
-        }
-    });
-   
+    // $('.navbar-nav li a').each(function(index) {
+    //     if ($(this).attr('href') === "."+window.location.pathname) {
+    //         $(this).parent().addClass("active");
+    //     }
+    // });
+var url = window.location;
+// Will only work if string in href matches with location
+
+   $('.navbar-nav li a').filter(function() {
+    return this.href == url;
+}).parent().addClass('active');
+
 });
