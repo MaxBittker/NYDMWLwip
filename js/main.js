2277aa51661f0c2f43e9d715688e39dbc8c1071c
max bittker <maxbittker@gmail.com>
2015-09-03 23:16:47 -0400

    js change and image rename

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.navbar-nav li a').each(function(index) {
-        if ($(this).attr('href') === window.location.pathname) {
+        if ($(this).attr('href') === "."+window.location.pathname) {
             $(this).parent().addClass("active");
         }
     });
